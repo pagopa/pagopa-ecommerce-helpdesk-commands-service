55fa3187a9cc3efc689a137c8c77174510600b6b
EmanueleBVtech <emanuele.serrao@bvtech.com>
2024-07-26 17:26:20 +0200

    fix: removed MVC dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.springframework.boot:spring-boot-starter-web-services")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.openapitools:jackson-databind-nullable:0.2.6")
   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
