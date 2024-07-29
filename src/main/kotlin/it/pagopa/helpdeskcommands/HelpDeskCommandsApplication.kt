93ba277514c5b98d439870cc38a9c360bb31d82c
s.caminati <simone.caminati@bvtech.com>
2024-07-29 10:49:32 +0200

    fix: apply spotless on init project

--- src/main/kotlin/it/pagopa/helpdeskcommands/HelpDeskCommandsApplication.kt
+++ src/main/kotlin/it/pagopa/helpdeskcommands/HelpDeskCommandsApplication.kt
@@ -4,9 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import reactor.core.publisher.Hooks
 
-
-@SpringBootApplication
-class HelpDeskCommandsApplication
+@SpringBootApplication class HelpDeskCommandsApplication
 
 fun main(args: Array<String>) {
     Hooks.enableAutomaticContextPropagation()
