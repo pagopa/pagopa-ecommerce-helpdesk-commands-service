47232f501ce72c9943f3274560e31bdfd126c076
Dylan Tangredi <dylan.tangredi@bvtech.com>
2024-10-09 11:14:59 +0200

    feat: add url to psp mapping from commons library

--- src/main/kotlin/it/pagopa/helpdeskcommands/utils/RedirectKeysConfiguration.kt
+++ src/main/kotlin/it/pagopa/helpdeskcommands/utils/RedirectKeysConfiguration.kt
@@ -1,8 +1,7 @@
 package it.pagopa.helpdeskcommands.utils
 
-import arrow.core.Either;
-import arrow.core.left;
-import arrow.core.right;
+import arrow.core.Either
+import arrow.core.right
 import it.pagopa.helpdeskcommands.exceptions.RedirectConfigurationException
 import it.pagopa.helpdeskcommands.exceptions.RedirectConfigurationType
 import java.net.URI
@@ -11,14 +10,15 @@ import java.util.function.Predicate
 import java.util.stream.Collectors
 
 /**
- * This class manages the PSP redirect URL map.
+ * This class manages the PSP redirect URL map. It has been converted to Kotlin from the
+ * ecommerce-commons Java class
  */
 class RedirectKeysConfiguration(
     pspUrlMapping: Map<String?, String?>,
     paymentTypeCodeList: Set<String>
 ) {
     private val redirectBeApiCallUriMap: Map<String, URI>
-    
+
     init {
         val redirectUriMap: MutableMap<String, URI> = HashMap()
         // URI.create throws IllegalArgumentException that will prevent module load for
@@ -26,13 +26,15 @@ class RedirectKeysConfiguration(
         pspUrlMapping.forEach { (pspId: String?, uri: String?) ->
             redirectUriMap[pspId!!] = uri?.let { URI.create(it) }!!
         }
-        val missingKeys = paymentTypeCodeList
-            .stream()
-            .filter(Predicate.not { key: String -> redirectUriMap.containsKey(key) })
-            .collect(Collectors.toSet())
+        val missingKeys =
+            paymentTypeCodeList
+                .stream()
+                .filter(Predicate.not { key: String -> redirectUriMap.containsKey(key) })
+                .collect(Collectors.toSet())
         if (missingKeys.isNotEmpty()) {
             throw RedirectConfigurationException(
-                "Misconfigured redirect.pspUrlMapping, the following redirect payment type code b.e. URIs are not configured: $missingKeys",
+                "Misconfigured redirect.pspUrlMapping, " +
+                    "the following redirect payment type code b.e. URIs are not configured: $missingKeys",
                 RedirectConfigurationType.BACKEND_URLS
             )
         }
@@ -40,14 +42,13 @@ class RedirectKeysConfiguration(
     }
 
     /**
-     * Returns a PSP redirect URL given a key by searching among the configured
-     * URLs.
+     * Returns a PSP redirect URL given a key by searching among the configured URLs.
      *
-     * @param touchpoint      the touchpoint used to initiate the transaction
-     * @param pspId           the psp id chosen for the current
+     * @param touchpoint the touchpoint used to initiate the transaction
+     * @param pspId the psp id chosen for the current
      * @param paymentTypeCode redirect payment type code
-     * @return Either valued with a PSP redirect URI, if valid, or exception for
-     *         invalid key param or bad configuration
+     * @return Either valued with a PSP redirect URI, if valid, or exception for invalid key param
+     *   or bad configuration
      */
     fun getRedirectUrlForPsp(
         touchpoint: String,
@@ -61,26 +62,29 @@ class RedirectKeysConfiguration(
          */
         val searchResult = searchRedirectUrlForPsp(touchpoint, pspId, paymentTypeCode)
 
-        return searchResult?.right() ?: Either.Left(
-            RedirectConfigurationException(
-                "Missing key for redirect return url with following search parameters: " +
+        return searchResult?.right()
+            ?: Either.Left(
+                RedirectConfigurationException(
+                    "Missing key for redirect return url with following search parameters: " +
                         "touchpoint: [$touchpoint] pspId: [$pspId] paymentTypeCode: [$paymentTypeCode]",
-                RedirectConfigurationType.BACKEND_URLS
+                    RedirectConfigurationType.BACKEND_URLS
+                )
             )
-        )
     }
 
     /**
-     * Execute a recursive search on the redirectBeApiCallUriMap. The recursion
-     * method will be called with the key without the first parameter element.
-     * The method has been converted to Kotlin from the ecommerce-commons Java library.
+     * Execute a recursive search on the redirectBeApiCallUriMap. The recursion method will be
+     * called with the key without the first parameter element. The method has been converted to
+     * Kotlin from the ecommerce-commons Java library.
      *
      * @param params List of parameters that compose the key.
      * @return The found URI or an empty value.
      */
     private fun searchRedirectUrlForPsp(vararg params: String): URI? {
         val key = params.joinToString("-")
-        redirectBeApiCallUriMap[key]?.let { return it }
+        redirectBeApiCallUriMap[key]?.let {
+            return it
+        }
 
         return if (params.size > 1) {
             searchRedirectUrlForPsp(*params.sliceArray(1 until params.size))
