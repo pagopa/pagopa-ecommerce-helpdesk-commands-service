88455e4e5c3fe90af865d724196a03a6a74d2f57
Francesco Di Tonno <francesco.ditonno@bvtech.com>
2024-10-11 12:56:50 +0200

    PPABV-7 business logic per esecuzione storno redirect psp tramite forwarder

---
+++ src/main/kotlin/it/pagopa/helpdeskcommands/config/RedirectConfigurationBuilder.kt
@@ -0,0 +1,43 @@
+package it.pagopa.helpdeskcommands.config
+
+import it.pagopa.helpdeskcommands.utils.RedirectKeysConfiguration
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Configuration class used to read all the PSP configurations that will be used during redirect
+ * transaction
+ */
+@Configuration
+class RedirectConfigurationBuilder {
+    /**
+     * Create a {@code RedirectKeysConfiguration} that will contain to every handled PSP, the
+     * backend URI to be used to perform Redirect payment flow api call, then provides a method to
+     * search based on the touchpoint key - pspId - paymentTypeCode keys
+     *
+     * @param paymentTypeCodes
+     * - set of all redirect payment type codes to be handled flow
+     *
+     * @param pspUrlMapping
+     * - configuration parameter that contains PSP to URI mapping
+     *
+     * @return a configuration map for every PSPs
+     */
+    @Bean
+    fun redirectBeApiCallUriConf(
+        @Value("#{\${redirect.pspUrlMapping}}") pspUrlMapping: Map<String, String>,
+        @Value("\${redirect.paymentTypeCodes}") paymentTypeCodes: Set<String>
+    ): RedirectKeysConfiguration {
+        // URI.create throws IllegalArgumentException that will prevent module load for
+        // invalid PSP URI configuration
+        // the redirect url configuration map is in common and it's used to configure both
+        // redirections
+        // and redirections/refunds endpoints. here we want to configure refunds endpoint only since
+        // it's the only api call that will be performed by event dispatcher for redirections
+        // payment
+        // flow
+        val redirectUriMap = pspUrlMapping.mapValues { "${it.value}/refunds" }
+        return RedirectKeysConfiguration(redirectUriMap, paymentTypeCodes)
+    }
+}
--- src/main/kotlin/it/pagopa/helpdeskcommands/services/CommandsService.kt
+++ src/main/kotlin/it/pagopa/helpdeskcommands/services/CommandsService.kt
@@ -1,26 +1,105 @@
 package it.pagopa.helpdeskcommands.services
 
+import it.pagopa.generated.helpdeskcommands.model.RefundRedirectRequestDto
+import it.pagopa.generated.helpdeskcommands.model.RefundRedirectResponseDto
 import it.pagopa.generated.npg.model.RefundResponseDto
+import it.pagopa.helpdeskcommands.client.NodeForwarderClient
 import it.pagopa.helpdeskcommands.client.NpgClient
+import it.pagopa.helpdeskcommands.exceptions.BadGatewayException
+import it.pagopa.helpdeskcommands.exceptions.NodeForwarderClientException
 import it.pagopa.helpdeskcommands.exceptions.NpgClientException
+import it.pagopa.helpdeskcommands.exceptions.RefundNotAllowedException
 import it.pagopa.helpdeskcommands.utils.NpgApiKeyConfiguration
 import it.pagopa.helpdeskcommands.utils.PaymentMethod
+import it.pagopa.helpdeskcommands.utils.RedirectKeysConfiguration
 import it.pagopa.helpdeskcommands.utils.TransactionId
 import java.math.BigDecimal
 import java.util.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
 
 @Service
 class CommandsService(
     @Autowired private val npgClient: NpgClient,
-    @Autowired private val npgApiKeyConfiguration: NpgApiKeyConfiguration
+    @Autowired private val npgApiKeyConfiguration: NpgApiKeyConfiguration,
+    @Autowired private val redirectKeysConfiguration: RedirectKeysConfiguration,
+    @Autowired
+    private val nodeForwarderClient:
+        NodeForwarderClient<RefundRedirectRequestDto, RefundRedirectResponseDto>
 ) {
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    fun requestRedirectRefund(
+        transactionId: TransactionId,
+        touchpoint: String,
+        pspTransactionId: String,
+        paymentTypeCode: String,
+        pspId: String
+    ): Mono<RefundRedirectResponseDto> {
+        return redirectKeysConfiguration
+            .getRedirectUrlForPsp(touchpoint, pspId, paymentTypeCode)
+            .fold(
+                { ex -> Mono.error(ex) },
+                { uri ->
+                    logger.info("Processing Redirect transaction refund. ")
+                    nodeForwarderClient
+                        .proxyRequest(
+                            request =
+                                RefundRedirectRequestDto()
+                                    .action("refund")
+                                    .idPSPTransaction(pspTransactionId)
+                                    .idTransaction(transactionId.value()),
+                            proxyTo = uri,
+                            requestId = transactionId.value(),
+                            responseClass = RefundRedirectResponseDto::class.java
+                        )
+                        .onErrorMap(NodeForwarderClientException::class.java) { exception ->
+                            val errorCause = exception.cause
+                            val httpErrorCode: Optional<HttpStatus> =
+                                Optional.ofNullable(errorCause).map {
+                                    (if (it is WebClientResponseException) {
+                                        it.statusCode
+                                    } else {
+                                        null
+                                    })
+                                        as HttpStatus?
+                                }
+                            logger.error(
+                                "Error performing Redirect refund operation for transaction with id: [${transactionId.value()}]. psp id: [$pspId], pspTransactionId: [$pspTransactionId], paymentTypeCode: [$paymentTypeCode], received HTTP response error code: [${
+                            httpErrorCode.map { it.toString() }.orElse("N/A")
+                        }]",
+                                exception
+                            )
+                            httpErrorCode
+                                .map {
+                                    val errorCodeReason =
+                                        "Error performing refund for Redirect transaction with id: [${transactionId.value()}] and payment type code: [$paymentTypeCode], HTTP error code: [$it]"
+                                    if (it.is5xxServerError) {
+                                        BadGatewayException(errorCodeReason)
+                                    } else {
+                                        RefundNotAllowedException(
+                                            transactionId,
+                                            errorCodeReason,
+                                            exception
+                                        )
+                                    }
+                                }
+                                .orElse(
+                                    BadGatewayException(
+                                        "Error performing refund for Redirect transaction with id: [${transactionId.value()}] and payment type code: [$paymentTypeCode]"
+                                    )
+                                )
+                        }
+                        .map { it.body }
+                }
+            )
+    }
 
     fun requestNpgRefund(
         operationId: String,
