7f06b4dc1b11d0910b51548269d64ebb7adecf4b
sfarralorenzo <lorenzo.sfarra@pagopa.it>
2025-05-30 11:17:58 +0200

    chore: add id java

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   kotlin("jvm") version "1.9.25"
   kotlin("plugin.spring") version "1.9.25"
+  id("java")
   id("org.springframework.boot") version "3.3.5"
   id("io.spring.dependency-management") version "1.1.6"
   id("org.openapi.generator") version "6.3.0"
