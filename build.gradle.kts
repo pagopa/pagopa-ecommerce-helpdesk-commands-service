90b63134c6b02f46534d4dffe8453f900bc2a3dd
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-09-04 10:18:50 +0200

    feat(gradle): remove unused task

--- build.gradle.kts
+++ build.gradle.kts
@@ -243,12 +243,6 @@ tasks.withType<KotlinCompile> {
   // kotlinOptions.jvmTarget = "21"
 }
 
-tasks.register("printCommonsVersion") {
-  description = "Prints the referenced commons library version."
-  group = "commons"
-  doLast { print(Deps.ecommerceCommonsVersion) }
-}
-
 tasks.test {
   useJUnitPlatform()
   finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
